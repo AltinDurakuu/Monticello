ca18e4fb89843320bf918f7856866a2f192c07d0
AltinDurakuu <altinduraku404@gmail.com>
2021-04-18 16:00:34 +0200

    Last commit

--- main.js
+++ main.js
@@ -18,4 +18,26 @@ $(document).ready(function(){
         slidesToScroll: 1
         });
   });
-  
+  function initMap() {
+    let ourLocation = {
+        lat: 42.661911567240736,
+        lng: 21.162545989865308
+    }
+  let map = new google.maps.Map(document.getElementById('map'),{
+    center: ourLocation,
+    zoom: 10
+  });
+
+  let iconURL = 'assets/location-icon-for-map.png';
+  let icon = {
+      url: iconURL,
+      scaledSize: new google.maps.Size(100, 100)
+  };
+
+
+    let marker = new google.maps.Marker({
+      map: map,
+      icon: icon,
+      position: ourLocation
+    });
+}
