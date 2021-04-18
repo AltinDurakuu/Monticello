42ab3000ac27b87d4babd2e674b3c52870ff8ddd
AltinDurakuu <altinduraku404@gmail.com>
2021-04-18 14:01:06 +0200

    Final commit

--- main.js
+++ main.js
@@ -8,4 +8,14 @@ $(document).ready(function(){
     arrows: false,
     slidesToScroll: 1
     });
+    $('.pojects-slider-container').slick({
+        dots: true,
+        infinite: true,
+        autoplay:true,
+        autoplaySpeed: 2000,
+        arrows: false,
+        slidesToShow: 3,
+        slidesToScroll: 1
+        });
   });
+  
